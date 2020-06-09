0bd542fe8d546efc92d905025bb14170d12c5dc2
我的Pro <18006125586@189.cn>
2020-06-09 19:32:31 +0800

    创建完项目

---
+++ 菜谱/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  菜谱
+//
+//  Created by LUOSU on 2020/6/9.
+//  Copyright © 2020 LUOSU. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ 菜谱/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  菜谱
+//
+//  Created by LUOSU on 2020/6/9.
+//  Copyright © 2020 LUOSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
